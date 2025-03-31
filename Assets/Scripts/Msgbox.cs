7d432fa79ab84d63cd42a8574b24ddc98df1bd0a
qing_yun_fang <hcsoftcn@126.com>
2025-03-31 16:00:52 +0800

    完成了注册用户模块

---
+++ Assets/Scripts/Msgbox.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Localization.Components;
+
+public class Msgbox : MonoBehaviour
+{
+    public LocalizeStringEvent m_Localize;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //m_Localize=transform.Find("msg").GetComponent<LocalizeStringEvent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetMsg(string msg)
+    {
+        m_Localize.SetEntry(msg);
+    }
+    public void Show(bool vis)
+    {
+        gameObject.SetActive(vis);
+    }
+    public void OnBtnOK()
+    {
+        gameObject.SetActive(false);
+    }
+}
--- Assets/Scripts/RegUI.cs
+++ Assets/Scripts/RegUI.cs
@@ -2,18 +2,22 @@
 using GameDB;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEditor;
 
 public class RegUI : NetworkBehaviour
 {
     public InputField user;
     public InputField pass;
     public InputField pass1;
+    private Msgbox msgbox;
     // Start is called before the first frame update
     void Start()
     {
-        //测试中文编码
-        //if (IsServer)
-            //GetComponent<NetworkObject>().Spawn();
+        GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/MsgBox.prefab");
+        GameObject instance = Instantiate(prefab);
+        instance.transform.position = new Vector3(0, 0, 0);
+        instance.transform.rotation = Quaternion.identity;
+        msgbox = instance.GetComponent<Msgbox>();
     }
 
     // Update is called once per frame
@@ -29,10 +33,25 @@ public class RegUI : NetworkBehaviour
 
     public void OnBtnSubmit()
     {
-        //Debug.LogFormat("{0},{1}", pass.text, pass1.text);
+        //用户名不能为空
+        if(user.text.Trim()=="")
+        {
+            msgbox.SetMsg("reg.msg5");
+            msgbox.Show(true);
+            return;
+        }
+        //密码必须大于6位
+        if(pass.text.Trim().Length<6)
+        {
+            msgbox.SetMsg("reg.msg6");
+            msgbox.Show(true);
+            return;
+        }    
+        //二次输入密码是否一致
         if(pass.text!=pass1.text)
         {
-            Debug.Log("两次输入的密码不一致。");
+            msgbox.SetMsg("reg.msg2");
+            msgbox.Show(true);
             return;
         }
         RegUserServerRpc(user.text,pass.text);
@@ -45,14 +64,15 @@ public class RegUI : NetworkBehaviour
         db.OpenDB();
         if (db.IsUserExists(user))
         {
+            //注册失败，用户名已经存在。
             ResClientRpc(-1);
         }
         else
         {
             if (db.RegUser(user, pass))
-                ResClientRpc(0);
+                ResClientRpc(0);//注册成功。
             else
-                ResClientRpc(-2);
+                ResClientRpc(-2);//注册失败，系统故障。
         }
         db.CloseDB();
     }
@@ -61,10 +81,22 @@ public class RegUI : NetworkBehaviour
     public void ResClientRpc(int status)
     {
         if (status == 0)
-            Debug.Log("注册成功。");
+        {
+            //注册成功
+            msgbox.SetMsg("reg.msg4");
+            msgbox.Show(true);
+        }
         else if (status == -1)
-            Debug.Log("注册失败，用户名已经存在。");
+        {
+            //注册失败，用户名已经存在。
+            msgbox.SetMsg("reg.msg1");
+            msgbox.Show(true);
+        }
         else
-            Debug.Log("注册失败，系统故障。");
+        {
+            //注册失败，系统故障。
+            msgbox.SetMsg("reg.msg3");
+            msgbox.Show(true);
+        }
     }
 }
